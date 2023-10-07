5ef5f7ae51cd90433fde0ebbe8e7d2a3f0492789
akiumarsi <a.kiumarsi.ir@gmail.com>
2023-10-07 21:06:56 +0330

    add guess number and score

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+const scoreContent = document.querySelector('.score');
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guessValue = Number(document.querySelector('.guess').value);
+  let message = document.querySelector('.message');
+
+  if (!guessValue) {
+    message.textContent = 'no number';
+  } else if (guessValue === secretNumber) {
+    message.textContent = 'correct number';
+  } else if (guessValue > secretNumber) {
+    if (score > 1) {
+      message.textContent = 'too high';
+      score--;
+      scoreContent.textContent = score;
+    } else {
+      message.textContent = 'you lose';
+      score = 0;
+      scoreContent.textContent = score;
+    }
+  } else if (guessValue < secretNumber) {
+    if (score > 1) {
+      message.textContent = 'tot low';
+      score--;
+      scoreContent.textContent = score;
+    } else {
+      message.textContent = 'you lose';
+      score = 0;
+      scoreContent.textContent = score;
+    }
+  }
+});
